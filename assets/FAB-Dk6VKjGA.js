b8f74bf094a0ac21d92b07ef4ed4f54e9a70419d
exho-g <exho-g@users.noreply.github.com>
2025-04-07 07:30:24 +0000

    deploy: exho-g/budgy@89b296b4cd03176fbe286d01d37eb192d1154d48

---
+++ assets/FAB-Dk6VKjGA.js
@@ -0,0 +1,11 @@
+import{J as e,j as o,o as t,c as a,Q as c,R as n}from"./index-3qMzOsUU.js";/**
+ * @license lucide-vue-next v0.484.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=e("plus",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]]);/**
+ * @license lucide-vue-next v0.484.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=e("x",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]),r={class:"btn btn-circle btn-primary fixed bottom-22 shadow-primary/50 shadow-md right-4 btn-xl"},m={__name:"FAB",props:["icon"],setup(s){return(i,p)=>(t(),o("button",r,[(t(),a(c(n(s.icon)),{size:36,strokeWidth:"2"}))]))}};export{d as P,l as X,m as _};
