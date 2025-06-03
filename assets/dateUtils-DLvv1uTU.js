f73068cb6a8c476e3d09170fda0c5d5c1e9847c2
exho-g <exho-g@users.noreply.github.com>
2025-06-03 07:40:04 +0000

    deploy: exho-g/budgy@5525e276e8f608c8ca925e1690267d8f36eaaebb

--- assets/dateUtils-DLvv1uTU.js
+++
@@ -1,6 +0,0 @@
-import{c}from"./index-CvJCmTcD.js";/**
- * @license lucide-vue-next v0.485.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const d=c("wallet",[["path",{d:"M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4h-3a2 2 0 0 0 0 4h3a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1",key:"18etb6"}],["path",{d:"M3 5v14a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1v-4",key:"xoc0q4"}]]),g=a=>{const t=new Date(a);if(isNaN(t.getTime()))throw new Error("Invalid date");const e=t.getFullYear(),r=String(t.getMonth()+1).padStart(2,"0"),o=String(t.getDate()).padStart(2,"0"),n=String(t.getHours()).padStart(2,"0"),s=String(t.getMinutes()).padStart(2,"0");return`${e}-${r}-${o}T${n}:${s}`};export{d as W,g as f};
